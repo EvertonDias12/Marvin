77ff189ed7c18c79478bc656b055fc7a47fc2fef
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-01-14 11:56:47 -0300

    Aula 1

---
+++ fase01/ex02/nomes.js
@@ -0,0 +1,22 @@
+function mostrarNome(){
+   const nome = "Doutor Estranho";
+   console.log(nome);
+}
+
+mostrarNome()
+
+function nomeDeVolta(){
+    const name = "O andróide paranóide";
+    console.log(name);
+}    
+    
+nomeDeVolta()
+
+
+function nomes(nome1, nome2){
+    const nome = ("Thor " + "e " + " Loki")
+    console.log(nome);
+}
+
+nomes()
+
