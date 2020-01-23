c5c08a473f9b4fd8e6f316a6fa0335b7d091c3be
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-01-23 10:11:59 -0300

    exibir o primeiro caracter em minusculo

--- fase02/ex04/minusculo.js
+++ fase02/ex04/minusculo.js
@@ -1,6 +1,5 @@
 function minusculo(palavra){
-    var palavras = palavra;
-    return palavras.substring(0,1).concat(palavras.substring(1).toUpperCase())
+    return palavra.substring(1,0).toLowerCase();
 }
 
-
+ 
