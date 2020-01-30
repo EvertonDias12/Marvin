53e9f9118bb706906ddbd9e4fee0519b46625af7
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-01-30 08:55:53 -0300

    ambos sao pares ou nao

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,14 +1,13 @@
 function pares(num1, num2){
-    var numero = num1;
-    var numero1 = num2;
-    var resto1 = numero % 2
-    var resto2 = numero1 %2
-    var total = resto1, resto2
+    var resto1 = num1 % 2
+    var resto2 = num2 % 2
+    var total = resto1 || resto2
     
     if(total == 0){
-        return "Os números sao pares"
+        return "Os numeros sao pares"
     }else{
-        return "Os números nao sao pares"
+        return "Os numeros nao sao pares"
     }
 }
+
 
