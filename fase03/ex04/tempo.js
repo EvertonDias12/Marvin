6091614f18503cf27007af23b2732902445fd6e1
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-02-06 08:53:56 -0300

    calculando diferenca de datas

---
+++ fase03/ex04/tempo.js
@@ -0,0 +1,24 @@
+function tempo(data1, data2, unidade){
+    var dataIni = new Date(data1)
+    var dataFim = new Date(data2)
+    
+    switch(unidade){
+    case "dia":
+        var periodo = Math.abs(dataIni.getTime() - dataFim.getTime())/1000/60/60/24
+        return Math.round(periodo)
+        break;
+        
+    case "mes":
+        var perido = Math.abs(dataIni.getTime() - dataFim.getTime())/1000/60/60/24/30
+        return Math.round(perido)
+        break;
+    
+    case "ano":
+        var perido = Math.abs(dataIni.getTime() - dataFim.getTime())/1000/60/60/24/30/12
+        return Math.round(perido)
+        break;
+        
+    
+    }
+}
+
