3580ffcb7af85bd84ce93c231ffc1bf1818e61d5
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-02-06 11:18:02 -0300

    sequencia apartir de 0

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,16 @@
+function ateZero(numero){
+    var i = 0;
+        while(i <= numero){
+        console.log(i)
+        i++
+        }
+        
+        if(numero <= 0){
+            while(i >= numero){
+            console.log(numero)
+            numero++
+            }
+        }
+        
+    }
+
