e3fd8aeb032cdbb61744cb7ad170c09f90271c20
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-02-11 11:12:53 -0300

    fizz e buzz

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -2,21 +2,14 @@ function fizzbuzz(num1, num2){
    
     while(num1 <= num2){
         if(num1 % 3 === 0 && num1 % 5 === 0){
-            console.log(num1 + " FizzBuzz")
-            num1++
+            console.log(num1 + " FizzBuzz");
+        }else if(num1 % 5 === 0){
+            console.log(num1 + " Buzz");
+        }else if(num1 % 3 === 0){
+            console.log(num1 + " Fizz");
+        }else{
+            console.log(num1); 
         }
-        
-        if(num1 % 5 === 0){
-            console.log(num1 + " Buzz")
-            num1++
-        }
-        
-        if(num1 % 3 === 0){
-            console.log(num1 + " Fizz")
-            num1++
-            
-        }
-        console.log(num1)
         num1++
     }
 }
