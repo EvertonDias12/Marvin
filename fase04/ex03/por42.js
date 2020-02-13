b00b5a708d3bd84d37a7b8a5340f02dddacde437
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-02-13 08:03:03 -0300

    por 42

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -2,14 +2,13 @@ function por42(num1, num2){
     var result = null;
 	while(num1 <= num2){
 	    if(num1 % 42 === 0){
-	        return Math.max(num1);
+	        return Math.max(num1) + 42;
 	    }
 	   num1++;
 	}
 	
 	if(!result){
 	        console.log("Não encontrado");
-	         return false;
+	        return false;
 	    }
 }
-
