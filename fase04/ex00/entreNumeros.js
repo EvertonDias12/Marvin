f163c4e731801946a9f3eb8cb798f4d2f7c1f0ef
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-02-13 08:47:49 -0300

    por 42

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,11 +1,22 @@
-function entreNumeros(min, max){
-    var i = 0;
-    
-    while(min <= max){
-        console.log(min, max)
-        min++
-    }
+function por42(num1, num2){
+    var result = null;
+    var contador = 0;
+	while(num1 <= num2){
+	    if(num1 % 42 === 0){
+	        contador = contador + 1;
+	    }
+	
+	if(contador === 2){
+	    return(num1);
+	    
+	}
+	num1++
+	
+	if(!result){
+	        console.log("Não encontrado");
+	        return false;
+	    }
+	}
 }
-
 
 
