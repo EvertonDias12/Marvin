77947e79f27e32d0699dcb7718d2e459daf47a38
EvertonDias12 <47272636+EvertonDias12@users.noreply.github.com>
2020-02-20 11:02:49 -0300

    trocar altura por idade

---
+++ fase06/ex02/animais.js
@@ -0,0 +1,18 @@
+function animais(){
+    var obj = array;
+    var novaIdade = 5;
+    var i = 0;
+    
+    while(i < array.length){
+        var posicao = obj[i];
+        delete posicao.altura;
+        if(posicao.idade !== null){
+            posicao.idade = novaIdade
+        }
+        novaIdade++
+        i++
+    }
+    return array;
+}
+
+
